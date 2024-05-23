b15360e95522e9d5befbaeb8206a45dc0c86828d
Omar Nabil <154880401+omarnabil986@users.noreply.github.com>
2024-05-23 14:31:26 +0300

    Add files via upload
    Built this project based on a udemy course

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '976747df-71ec-4aa0-89d1-43bba41075d2';
+export const MODAL_CLOSE_SEC = 2.5;
